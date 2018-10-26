8d428d333f27530b019716507c0c8c22b9e3f2e5
Vladimir Tambovtsev <tambovcev99@mail.ru>
2018-10-26 19:18:38 +0300

    add title to iframe

--- src/components/location/index.js
+++ src/components/location/index.js
@@ -4,6 +4,7 @@ const index = () => {
 	return (
 		<div className="location_wrapper">
 			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24219.60999177894!2d-73.97643409069018!3d40.64198229187558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25b493b5ae649%3A0xfbaa9e9d6f1ddde0!2sKings+Theatre!5e0!3m2!1sru!2sru!4v1540567701319"
+				title="Unique title for rendering"
 				width="100%"
 				height="500px"
 				frameBorder="0"
