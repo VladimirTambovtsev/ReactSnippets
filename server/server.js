51c791f5ad21f7bdcab5154a521fa701596f83ee
Vladimir Tambovtsev <tambovcev99@mail.ru>
2018-11-16 23:59:00 +0300

    fixed heroku server bug

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.use('/api/brands', brands)
 
 // default for heroku
 if (process.env.NODE_ENV === 'production') {
-  import path from 'path'
+  const path = require('path')
   app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
   })
